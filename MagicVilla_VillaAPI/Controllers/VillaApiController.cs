42b11ff1df37a6efcea108e304df7244c9ffb6a2
Alex Wu <ciic789@gmail.com>
2022-11-12 12:27:02 +1100

    add villa controller and villa model

---
+++ MagicVilla_VillaAPI/Controllers/VillaApiController.cs
@@ -0,0 +1,11 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        public IEnumerable<Villa>
+    }
+}
